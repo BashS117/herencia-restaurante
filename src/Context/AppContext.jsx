c5419975dfb83e0bac87074ca8a0c933adabf96c
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-12-05 18:17:41 -0500

    feat to add products to cart

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -7,19 +7,26 @@ export const AppProvider =({children})=>{
     const initialState={
         panelOpen: false,
         selectedCategory: null, // Nuevo campo para almacenar la categoría seleccionada
+        cart: [], //elementos del carrito
 
     }  
 
-    const reducerObject=(state,category)=>({
+    const reducerObject=(state,payload)=>({
         'PANELOPEN':{
             ...state,
-            panelOpen: state.selectedCategory !== category || !state.panelOpen,
-          selectedCategory: category,
+            panelOpen: state.selectedCategory !== payload || !state.panelOpen,
+          selectedCategory: payload,
+        },  
+        'ADD_TO_CART': {
+            ...state,
+            cart:[...state.cart,payload],//añadir el elemento al carrito
         },
-    
-       
-    }
-    )
+        'REMOVE_FROM_CART': {
+            ...state,
+            cart:state.cart.filter(item=>item.id !=payload.id)//eliminar elemento del carrito
+        }
+    })
+
 
     const reducer = (state,action)=>{
         if(reducerObject(state,action.payload)[action.type]){
@@ -33,13 +40,23 @@ export const AppProvider =({children})=>{
     
 console.log("state",state)
 
+//defino acciones relacionadas con el carrito 
+
+ const addToCart = (dispatch, item) => {
+    dispatch({ type: 'ADD_TO_CART', payload: item });
+  };
+  
+   const removeFromCart = (dispatch, item) => {
+    dispatch({ type: 'REMOVE_FROM_CART', payload: item });
+  };
 
 
 
 return (
     <AppContext.Provider value={
         {
-            state,dispatch
+            state,dispatch,
+            addToCart,removeFromCart
         }
     }>
 {children}
--- src/components/AcordionPanel/index.jsx
+++ src/components/AcordionPanel/index.jsx
@@ -22,9 +22,12 @@ const AcordionPanel = ({ products,category ,rightPanel}) => {
                   <p>ver mas...</p>
                </button>
               </div>
+              <button>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
+                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              </button>
+             
 
             </li>
           ))}
