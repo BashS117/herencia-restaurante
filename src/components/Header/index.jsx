0317271cac4fe43f4113003e46fe68c90e676c76
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-12-15 18:08:21 -0500

    ref style header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,18 +10,20 @@ const Header = () => {
             <figure>
                 <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/3d0cfc185580241.656649b978204.png" alt="" />
             </figure>
-            <div className='w-[65%] mt-[10px]'>
-                <h1>Corralazo</h1>
-                <p>Comidas Rapidas</p>
-                <p>Calle 5 #9-40 Corinto</p>
-                <p>Cel: 323 475 4284</p>
-                <ol className='text-[0.7rem]'>
+            <div className='w-[65%] mt-[10px] pl-[5px]'>
+                <h1 className='titlePage text-[2.8rem] text-orange'>Corralazo</h1>
+                <p className='subtitle'>Comidas Rapidas</p>
+                <div className='flex-col gap-1 flex items-end'>
+                    <p className='text-[0.8rem] bg-orange rounded-sm px-1 text-white  '>Calle 5 #9-40 Corinto</p>
+                    <p  className='text-[0.8rem]  bg-orange rounded-sm px-1 text-white '>Cel: 323 475 4284</p>
+                </div>
+                 <lo className='text-[0.7rem]'>
                     <li> Elige categoría y escoge tu producto</li>
                     <li> Envia tu pedido y espera tu domicilio</li>
-                </ol>
+                </lo>
             </div>
         </div>
-        <input className='w-[80%]' 
+        <input className='w-[80%] mt-2' 
         onChange={(event) => setFilter(event.target.value.trim() || null)}
         placeholder='Busca tu comida'
         type="search" />
