a51b599296f851075e4bbd4a891f189335686faa
Sebastian Rodriguez <sebasrm2000@gmail.com>
2024-08-13 23:34:42 -0500

    ref header style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,14 +13,15 @@ const Header = () => {
             <figure className='absolute top-8 left-3 shadow-md p-1 bg-white' >
                 <img className='w-[100px] h-[100px] ' src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/90afe3202162713.668196ce7fc88.png" alt="logo" />
             </figure>
-            <div className='mt-[2px] pl-[30%] text-start'>
+            <div className='mt-[2px] pl-[32%] text-start'>
                 <h1 className='titlePage text-[1.8rem]  text-black '>Herencia Restaurante</h1>
-                <div className='flex gap-1 items-end'>
+                
+                
+            </div>
+            <div className='flex gap-1 items-end justify-evenly'>
                     <p className='text-[0.8rem] bg-[#FFC107] rounded-sm px-1 text-white  '>Cra 8 #6-44 Corinto</p>
                     <p  className='text-[0.8rem]  bg-[#FFC107] rounded-sm px-1 text-white '>Cel: 322 533 4974</p>
                 </div>
-                
-            </div>
             <ul className='text-[0.7rem] px-4 justify-between flex'>
                     <li> 1. Elige categoría y escoge tu producto</li>
                     <li> 2. Envia tu pedido y espera tu domicilio</li>
